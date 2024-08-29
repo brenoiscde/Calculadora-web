85454cd627e8f6b8d8913d4a052ba19d15eb2289
brenoiscde <breninhoponticelli@gmail.com>
2024-08-29 20:14:41 -0300

    addnovosbotoes

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,10 @@
 function mostrarNum(num){
-    const nomeDaminhaCaixa = document.getElementById('label').innerHTML
-    document.getElementById('label').innerHTML = nomeDaminhaCaixa + num
+    const label = document.getElementById('label').innerHTML
+    document.getElementById('label').innerHTML = label + num
+
+    const historico = document.getElementById('historico').innerHTML
+    document.getElementById('historico').innerHTML =  historico + num
+    atualizarArrayNumeros()
 }
 function calcular(){
     let label = document.getElementById('label').innerHTML
@@ -17,4 +21,21 @@ function deletar(){
 }
 function reset(){
     document.getElementById('label').innerHTML = ""
+    document.getElementById('historico').innerHTML = ""
+}
+
+function alternarSinal() {
+    // Obtém o valor atual da label
+    let label = document.getElementById('label').innerHTML;
+    
+    // Verifica se a label não está vazia
+    if (label) {
+        // Se o valor começa com um "-", remove-o para tornar o número positivo
+        if (label.startsWith('-')) {
+            document.getElementById('label').innerHTML = label.substring(1);
+        } else {
+            // Caso contrário, adiciona um "-" no início para tornar o número negativo
+            document.getElementById('label').innerHTML = '-' + label;
+        }
+    }
 }
